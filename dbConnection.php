6ab0094289289359b4d0c5c736d9d4330c2974be
Relva Ardian <ardianrelva@gmail.com>
2024-09-26 13:55:57 +0700

    Add files via upload

---
+++ dbConnection.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'rawr_jago';
+$databaseUsername = 'ubuntu';
+$databasePassword = 'vockey';
+
+// Open a new connection to the MySQL server
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
